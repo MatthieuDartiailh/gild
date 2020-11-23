27a371d905ad72508c14acae3606c24373c73a0e
MatthieuDartiailh <marul@laposte.net>
2020-11-22 22:01:40 -0500

    plugins: reformat and add type annotations to packages plugin

--- glaze/plugins/packages/plugin.py
+++ glaze/plugins/packages/plugin.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
-# -----------------------------------------------------------------------------
-# Copyright 2015-2018 by Exopy Authors, see AUTHORS for more details.
+# --------------------------------------------------------------------------------------
+# Copyright 2020 by Glaze Authors, see git history for more details.
 #
 # Distributed under the terms of the BSD license.
 #
 # The full license is in the file LICENCE, distributed with this software.
-# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
 """Plugin handling the collection and registering of extension packages.
 
 """
-import pkg_resources
 import logging
 
-from atom.api import List, Dict
+import pkg_resources
+from atom.api import Dict, List
 from enaml.workbench.api import Plugin, PluginManifest
 
 from ...utils.traceback import format_exc
@@ -21,20 +20,16 @@ logger = logging.getLogger(__name__)
 
 
 class PackagesPlugin(Plugin):
-    """Plugin collecting and registering all manifest contributed by extension
-    packages.
+    """Collect and register all manifest contributed by extension packages."""
 
-    """
     #: Dictionary listing the extension packages registered at startup, each
     #: entries can contain either a dict listing the id of the registered
     #: manifest with a message indicating whether registering succeeded, or
     #: a message explaining why the package was not loaded.
     packages = Dict()
 
-    def stop(self):
-        """Unregister all manifest contributed by extension packages.
-
-        """
+    def stop(self) -> None:
+        """Unregister all manifest contributed by extension packages."""
         # Sort to respect the given priority when unregistering.
         heap = sorted(self._registered)
         for manifest_id in heap:
@@ -43,47 +38,42 @@ class PackagesPlugin(Plugin):
         self.packages.clear()
         self._registered = []
 
-    def collect_and_register(self):
-        """Iter over packages and register the manifest they are providing.
-
-        """
+    def collect_and_register(self) -> None:
+        """Iter over packages and register the manifest they are providing."""
         # Getting core plugin to signal errors.
-        core = self.workbench.get_plugin('enaml.workbench.core')
-        cmd = 'exopy.app.errors.signal'
+        core = self.workbench.get_plugin("enaml.workbench.core")
+        cmd = "glaze.errors.signal"
 
-        packages = dict()
-        registered = []
-        core.invoke_command('exopy.app.errors.enter_error_gathering', {})
-        for ep in pkg_resources.iter_entry_points('exopy_package_extension'):
+        packages: Dict[str, Dict[str, str]] = dict()
+        registered: List[PluginManifest] = []
+        core.invoke_command("glaze.errors.enter_error_gathering", {})
+        for ep in pkg_resources.iter_entry_points("exopy_package_extension"):
 
             # Check that all dependencies are satisfied.
             try:
                 ep.require()
             except Exception:
-                msg = 'Could not load extension package %s : %s'
+                msg = "Could not load extension package %s : %s"
                 msg = msg % (ep.name, format_exc())
                 packages[ep.name] = msg
-                core.invoke_command(cmd, dict(kind='package', id=ep.name,
-                                              message=msg))
+                core.invoke_command(cmd, dict(kind="package", id=ep.name, message=msg))
                 continue
 
             # Get all manifests
             packages[ep.name] = {}
             manifests = ep.load()()
             if not isinstance(manifests, list):
-                msg = 'Package %s entry point must return a list, not %s'
+                msg = "Package %s entry point must return a list, not %s"
                 msg = msg % (ep.name, str(type(manifests)))
                 packages[ep.name] = msg
-                core.invoke_command(cmd, dict(kind='package', id=ep.name,
-                                              message=msg))
+                core.invoke_command(cmd, dict(kind="package", id=ep.name, message=msg))
                 continue
 
             if any(not issubclass(m, PluginManifest) for m in manifests):
-                msg = 'Package %s entry point must only return PluginManifests'
+                msg = "Package %s entry point must only return PluginManifests"
                 msg = msg % ep.name
                 packages[ep.name] = msg
-                core.invoke_command(cmd, dict(kind='package', id=ep.name,
-                                              message=msg))
+                core.invoke_command(cmd, dict(kind="package", id=ep.name, message=msg))
                 continue
 
             for manifest in manifests:
@@ -91,24 +81,24 @@ class PackagesPlugin(Plugin):
                 try:
                     self.workbench.register(inst)
                 except ValueError:
-                    core.invoke_command(cmd,
-                                        dict(kind='registering', id=inst.id,
-                                             message=format_exc()))
+                    core.invoke_command(
+                        cmd, dict(kind="registering", id=inst.id, message=format_exc())
+                    )
                     continue
 
-                packages[ep.name][inst.id] = 'Successfully registered'
-                priority = getattr(inst, 'priority', 100)
+                packages[ep.name][inst.id] = "Successfully registered"
+                priority = getattr(inst, "priority", 100)
                 # Keep the insertion index, to avoid comparing id when
                 # sorting (it would make no sense).
                 registered.append((priority, len(registered), inst.id))
 
         self.packages = packages
         self._registered = registered
-        core.invoke_command('exopy.app.errors.exit_error_gathering', {})
+        core.invoke_command("glaze.errors.exit_error_gathering", {})
 
     # =========================================================================
     # --- Private API ---------------------------------------------------------
     # =========================================================================
 
     #: Private list of registered manifest used when stopping the plugin.
-    _registered = List()
+    _registered = List(PluginManifest)
